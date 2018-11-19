bda7e6a93ade4d29d86250004382dab47a1f733b
Lhance Adrian J. Rivera <lhanceadrianjrivera@Lhances-MacBook-Pro.local>
2018-11-19 22:37:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lhanceadrianjrivera/riveradelossantosproject/SignUpPage.java
@@ -0,0 +1,13 @@
+package com.example.lhanceadrianjrivera.riveradelossantosproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_page);
+    }
+}
